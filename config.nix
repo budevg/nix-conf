c185b3b0b1d1ec9757d2f1c85c34738719061c3c
Evgeny Budilovsky <budevg@gmail.com>
2018-09-14 12:40:04 +0300

    workaround issue when building qemu
    https://github.com/NixOS/nixpkgs/issues/38991

--- config.nix
+++ config.nix
@@ -6,7 +6,9 @@
 
     kvm-gtk = pkgs.qemu.overrideDerivation (attrs: {
       buildInputs = attrs.buildInputs ++ [ gtk3 gtk3.dev gettext ];
-      configureFlags = attrs.configureFlags ++ [ "--enable-gtk" "--target-list=x86_64-softmmu" ];
+      configureFlags = attrs.configureFlags ++ [
+        "--disable-docs" "--enable-gtk" "--target-list=x86_64-softmmu"
+      ];
       postInstall = attrs.postInstall + ''
         ; makeWrapper $out/bin/qemu-system-x86_64  $out/bin/kvm --set GTK_THEME Default --add-flags "-enable-kvm"
       '';
