799ca51273f7ab4a2b6c802b67dc846997ec2c44
Evgeny Budilovsky <budevg@gmail.com>
2018-09-14 12:39:42 +0300

    update scratch.sh to have size parameter

--- utils/scratch.sh
+++ utils/scratch.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-sudo mount -t tmpfs -o size=8G none ~/scratch/
+SCRATCH_PATH=`readlink -f ~/scratch`
+SIZE=${1-4G}
+
+if mountpoint -q $SCRATCH_PATH; then
+    sudo mount -o remount,size=$SIZE $SCRATCH_PATH
+else
+    sudo mount -t tmpfs -o size=$SIZE none $SCRATCH_PATH
+fi
