93bd43d3fac2d04b8a764bf90687f7f2dd4866af
Evgeny Budilovsky <budevg@gmail.com>
2020-04-26 22:06:09 +0300

    don't install stack by default

--- nixos/system.nix
+++ nixos/system.nix
@@ -4,7 +4,7 @@
   time.timeZone = "Asia/Jerusalem";
 
   environment.systemPackages = with pkgs; [
-    wget vim git bash axel stack python tightvnc
+    wget vim git bash axel python tightvnc
 
     trayer networkmanagerapplet polkit_gnome
     xdg-user-dirs xcompmgr synapse
