f55bc8f1d22731a514dd6dce5cd908bb3e81f7a4
Evgeny Budilovsky <budevg@gmail.com>
2020-10-09 11:55:13 +0300

    build emacs with jansson support

--- build/emacs-build.nix
+++ build/emacs-build.nix
@@ -8,6 +8,6 @@ stdenv.mkDerivation {
     gtk3-x11 xlibsWrapper xorg.libXaw xorg.libXpm xorg.libXft Xaw3d
     libpng libjpeg libungif libtiff librsvg libxml2
     gnutls ncurses imagemagick cairo lcms2 gpm dbus libselinux
-    m17n_lib libotf acl systemd
+    m17n_lib libotf acl systemd jansson
   ];
 }
