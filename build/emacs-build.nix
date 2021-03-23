96d17f60adb9fa49e0cd05bd6ce2e679ef7fff57
Evgeny Budilovsky <budevg@gmail.com>
2021-03-23 13:54:59 +0200

    emacs: native compilation support

--- build/emacs-build.nix
+++ build/emacs-build.nix
@@ -8,6 +8,16 @@ stdenv.mkDerivation {
     gtk3-x11 xlibsWrapper xorg.libXaw xorg.libXpm xorg.libXft Xaw3d
     libpng libjpeg libungif libtiff librsvg libxml2
     gnutls ncurses imagemagick cairo lcms2 gpm dbus libselinux
-    m17n_lib libotf acl systemd jansson
+    m17n_lib libotf acl systemd jansson libgccjit
   ];
+  LIBRARY_PATH = "${lib.getLib stdenv.cc.libc}/lib";
+  shellHook = ''
+    function compile-emacs-native {
+      ./autogen.sh && \
+      mkdir -p .build && \
+      cd .build &&  \
+      ../configure --with-native-compilation && \
+      time make NATIVE_FULL_AOT=1 $@
+    }
+  '';
 }
