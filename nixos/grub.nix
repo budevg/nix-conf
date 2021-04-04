a20eb184c0dd687e5c7cda736f8669dac0aa490d
Evgeny Budilovsky <budevg@gmail.com>
2021-04-04 21:57:31 +0300

    enable grub with uefi support

--- nixos/grub.nix
+++ nixos/grub.nix
@@ -4,6 +4,11 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
+  # enable for uefi
+  # boot.loader.grub.device = "nodev";
+  # boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.extraConfig = "terminal_input console; terminal_output console";
+  # enable for uefi
+  # boot.loader.efi.canTouchEfiVariables = true;
 }
