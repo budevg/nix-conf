2b1b544880c48ad6344f09d4dad408a37e3d2e2e
Evgeny Budilovsky <budevg@gmail.com>
2021-07-21 21:26:52 +0300

    provide dell fan packages

--- config.nix
+++ config.nix
@@ -7,6 +7,8 @@
     emacs-libvterm = callPackage ./pkgs/emacs-libvterm {};
     cscope-indexer = callPackage ./pkgs/cscope-indexer {};
     autopatchelf = callPackage ./pkgs/autopatchelf {};
+    dell-bios-fan-control = callPackage ./pkgs/dell-bios-fan-control {};
+    i8kutils = callPackage ./pkgs/i8kutils {};
 
     kvm-gtk = pkgs.qemu.overrideDerivation (attrs: {
       buildInputs = attrs.buildInputs ++ [ gtk3 gtk3.dev gettext ];
