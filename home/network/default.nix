80669ae41a5e0ff98f02c356a2d4503e13871a25
Evgeny Budilovsky <budevg@gmail.com>
2021-12-03 12:54:30 +0200

    add slack

--- home/network/default.nix
+++ home/network/default.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 
-{
-  home.file = {
-    ".ssh/config".source = ./.ssh/config;
-  };
+let unstable = import <unstable> { };
+in {
+  home.file = { ".ssh/config".source = ./.ssh/config; };
   home.packages = [
     pkgs.transmission-gtk
     pkgs.google-chrome
     pkgs.firefox
     pkgs.curl
     pkgs.tcpdump
+    unstable.slack
   ];
 }
