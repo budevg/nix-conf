41551afd97ae01cc6fadd27f453c28650b675654
Evgeny Budilovsky <budevg@gmail.com>
2022-02-12 12:55:41 +0200

    remove deprecated tightvnc

--- nixos/system.nix
+++ nixos/system.nix
@@ -4,7 +4,7 @@
   time.timeZone = "Asia/Jerusalem";
 
   environment.systemPackages = with pkgs; [
-    wget vim git bash axel python tightvnc
+    wget vim git bash axel python
 
     trayer networkmanagerapplet polkit_gnome
     xdg-user-dirs xcompmgr
