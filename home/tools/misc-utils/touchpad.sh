f3efcb19ac4758f41d267d76bce81f43a7a5813f
Evgeny Budilovsky <budevg@gmail.com>
2023-09-11 23:28:14 +0300

    fix touchpad disable script

--- home/tools/misc-utils/touchpad.sh
+++ home/tools/misc-utils/touchpad.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-DEV=`xinput | sed -nre '/TouchPad/s/.*id=([0-9]*).*/\1/p'`
+DEV=`xinput | sed -nre '/Touch[Pp]ad/s/.*id=([0-9]*).*/\1/p'`
 ENABLED=`xinput list-props $DEV | grep "Device Enabled" | cut -f3`
 xinput set-prop $DEV "Device Enabled" $((1 - $ENABLED))
