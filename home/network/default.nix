6960e142988eab29bc20056470c9a0dde8eaf8db
Evgeny Budilovsky <budevg@gmail.com>
2024-08-17 22:06:53 +0300

    firefox profiles

--- home/network/default.nix
+++ home/network/default.nix
@@ -6,9 +6,46 @@ in {
   home.packages = [
     pkgs.transmission-gtk
     pkgs.google-chrome
-    pkgs.firefox
     pkgs.curl
     pkgs.tcpdump
-    unstable.slack
+    pkgs.zoom-us
+    pkgs.slack
+    pkgs.telegram-desktop
   ];
+
+  programs.firefox = {
+    enable = true;
+
+    # about:policies#documentation
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DontCheckDefaultBrowser = true;
+    };
+
+    profiles = {
+      main = {
+        id = 0;
+        name = "main";
+        # https://kb.mozillazine.org/About:config_entries
+        settings = { };
+      };
+      hack = {
+        id = 1;
+        name = "hack";
+        # https://kb.mozillazine.org/About:config_entries
+        settings = {
+          "browser.aboutConfig.showWarning" = false;
+          "browser.aboutwelcome.didSeeFinalScreen" = true;
+          "browser.download.dir" = "${config.home.homeDirectory}/scratch";
+          "browser.download.folderList" = 2;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.quitShortcut.disabled" = true;
+          "extensions.activeThemeID" = "visionary-bold-colorway@mozilla.org";
+          "extensions.pocket.enabled" = false;
+          "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
+        };
+      };
+    };
+  };
 }
