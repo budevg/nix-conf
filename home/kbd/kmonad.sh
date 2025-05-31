1031df4c70b716f0502075fc813b5ed51f275883
Evgeny Budilovsky <budevg@gmail.com>
2025-05-31 17:35:57 +0300

    latitude 5531 keyboard config

--- home/kbd/kmonad.sh
+++ home/kbd/kmonad.sh
@@ -2,4 +2,4 @@
 
 dev=$(ls /dev/input/*/*-kbd | rofi -i -dmenu -p "Select keyboard device" -a 0 -no-custom)
 config=`readlink -f ~/.config/kmonad/config.kbd`
-sudo screen -d -m bash -c "sleep 1 && kmonad <(sed \"s|KBD_DEV|$dev|\" $config)"
+sudo env PATH=$PATH screen -d -m bash -c "sleep 1 && kmonad <(sed \"s|KBD_DEV|$dev|\" $config)"
